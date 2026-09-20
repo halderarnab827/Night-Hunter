1fc40b843bc7cdf1acc281b81009b547ccce4d05
ARNAB RAJU HALDER <halderarnab827@gmail.com>
2026-09-20 21:46:12 +0530

    Wire landing page and dashboard routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LandingPage from './components/LandingPage.jsx'
+
+function Root() {
+  return window.location.pathname === '/app' ? <App /> : <LandingPage />
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Root />
   </StrictMode>,
 )
