ec355f62f28a19d23cf58e0a20a0be09f8462e60
ARNAB RAJU HALDER <halderarnab827@gmail.com>
2026-09-20 20:19:42 +0530

    Add files via upload

---
+++ utils/colors.py
@@ -0,0 +1,14 @@
+# NIGHT HUNTER - Terminal Colors
+
+RESET = "\033[0m"
+
+BLUE = "\033[94m"
+RED = "\033[91m"
+CYAN = "\033[96m"
+GREEN = "\033[92m"
+YELLOW = "\033[93m"
+WHITE = "\033[97m"
+GRAY = "\033[90m"
+
+BOLD = "\033[1m"
+DIM = "\033[2m"
